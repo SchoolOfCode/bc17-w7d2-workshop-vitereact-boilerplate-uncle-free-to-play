0458d2d775c52afdf91013979e343bc1034157c3
Kellytheengineer <77806498+Kellytheengineer@users.noreply.github.com>
2024-08-13 13:11:30 +0200

    Your descriptive commit message

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,125 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Header(){
+  return(
+    <header className="header-container">
+        <h1>🔥 Fireplace Palace</h1>
+      </header> 
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+ <body>
+
+    <div className="wrapper">
+    <Header/>
+
+     
+      <main>
+        <section className="hero-section">
+          <img
+            src="/images/hero-mobile.png"
+            srcset="/images/hero-mobile.png 900w, /images/hero-desktop.png 901w"
+            sizes="(max-width: 900px) 100vw,
+             901px"
+            alt="Cozy living room with a fireplace"
+            className="hero-image"
+          />
+
+          <div className="hero-description">
+            <p className="hero-headline">
+              Discover the <br />
+              perfect fireplace ...
+            </p>
+            <p className="consultation-paragraph">
+              Book consultation: <span className="number-span">0121 345 6789</span>
+            </p>
+          </div>
+        </section>
+
+        <hr className="separator" />
+
+      
+        <section id="how-it-works" className="how-it-works-section">
+          <h2 className="how-it-works-text">How It Works</h2>
+          <div className="tiles">
+            <article className="tile">
+              <img
+                src="/images/how-it-works-1.png"
+                alt="Phone call illustration"
+                className="tile-image"
+              />
+              <h3>Give us a call...</h3>
+              <p>
+                Call us and book in a "Design Consultation" on a date and time
+                that suits you.
+              </p>
+            </article>
+            <article className="tile">
+              <img
+                src="/images/how-it-works-2.png"
+                alt="Home visit illustration"
+                className="tile-image"
+              />
+              <h3>We come to you...</h3>
+              <p>
+                We come to your home to do an assessment of the space and to
+                your style preference.
+              </p>
+            </article>
+            <article className="tile">
+              <img
+                src="/images/how-it-works-3.png"
+                alt="Fireplace recommendation illustration"
+                className="tile-image"
+              />
+              <h3>We recommend...</h3>
+              <p>We send you a bespoke set of fireplace recommendations.</p>
+            </article>
+          </div>
+        </section>
+      </main>
+
+      
+      <footer className="footer">
+        <div className="colunms-container">
+          <div className="footer-column">
+            <p>Find us on:</p>
+            <ul className="footer-nav">
+              <li>
+                <a href="https://www.facebook.com" className="socials-link">
+                  Facebook
+                  </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com" className="socials-link">
+                  Instagram
+                  </a>
+              </li>
+              <li>
+                <a href="https://www.tiktok.com" className="socials-link">TikTok</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <p>Contact us:</p>
+            <address>
+              <a href="mailto:info@firepalace.co.uk" className="email-link">
+                info@firepalace.co.uk
+                </a>
+            </address>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </body>
+
     </>
   )
 }
